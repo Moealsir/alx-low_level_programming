b80ea59dab70ed6d26d7d4e8d71228a7f2ad302c
Moealsir <mohamedwdalsir@gmail.com>
2023-09-12 13:38:41 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ *
+ * Description: function that prints the last digit of a number.
+ *
+ * @a:number to compute last digit
+ *
+ * Return: last digit
+ */
+
+
