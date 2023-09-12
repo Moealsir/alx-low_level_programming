1a7da4ed0341599a342925875900af19820bc4f7
Moealsir <mohamedwdalsir@gmail.com>
2023-09-12 13:36:38 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * _abs - Computers the absolute
+ * @c: The number computed
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	inf (n < 0)
+		n = (-1) * n;
+	return (0);
+}
