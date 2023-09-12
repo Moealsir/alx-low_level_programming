7ffc408f65e9b93933d50a65d9983e11f48d5da9
Moealsir <mohamedwdalsir@gmail.com>
2023-09-12 13:44:18 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,10 +6,15 @@
  * @c: The number computed
  * Return: Absolute value
  */
-
-int _abs(int n)
+int _abs(int c)
 {
-	if (n >= 0)
-		return (n);
-	return (-n);
+	if (c < 0)
+	{
+		int abs_val;
+
+
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
 }
