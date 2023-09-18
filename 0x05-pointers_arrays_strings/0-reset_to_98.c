31a750eedf47ae8dbd94dd8be24e53b1a461ceca
n <n>
2023-09-18 21:07:23 +0300

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - update value to 98
+ * 
+ * Return: Always 0.
+*/
+
+int main(){
+    int n;
+
+    n = 402;
+    printf("n=%d\n", n);
+    int *a = 98;
+    printf("n=%d", n);
+    return (0);
+}
