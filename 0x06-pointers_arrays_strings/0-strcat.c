c382d9445038cf915f716d39c0f61d2276581f5e
Moealsir <mohamedwdalsir@gmail.com>
2023-09-21 12:16:24 +0300

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+ * *_strcat - concatenates two strings.
+ * @dest: string 1
+ * @src: string 2
+ *
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat (*dest, *src);
+}
