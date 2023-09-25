faf7643a27d86afbe3b9a142b0d42f851d737f1e
Moealsir <mohamedwdalsir@gmail.com>
2023-09-25 21:10:59 +0300

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills s with constant b value n times
+ * @s: pointer
+ * @b: constant value
+ * @n: maximum number of filling
+ * Return: s 
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int inc; //increase untill n reach 0
+
+	for (inc = 0; n > 0; inc++)
+		s[inc] = b;
+
+	return (s);
+}
