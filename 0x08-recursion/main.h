9ace4ed1dd6aeefa81952104e35e4f5ae9bf96ec
Moealsir <mohamedwdalsir@gmail.com>
2023-09-28 11:46:53 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <string.h>
 
-int _putchar(char c);
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
