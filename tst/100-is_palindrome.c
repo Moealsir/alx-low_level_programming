4827b171701736764fa37849da3413f9f785bfb0
Moealsir <mohamedwdalsir@gmail.com>
2023-09-30 05:12:25 +0300

    tst

---
+++ tst/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int is_palindrome(char *s)
+{
+	int i, j;
+	
+	i = 0;
+	j = strlen(s) - 1;
+	while (i < j)
+	{
+		if (s[i] != s[j])
+			return (0);
+		i++;
+		j--;
+	}
+	return (1);
+}
+
