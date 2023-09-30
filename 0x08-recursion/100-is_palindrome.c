9c687b3edeafe3bed3081328cdc219904b618537
Moealsir <mohamedwdalsir@gmail.com>
2023-09-30 05:45:17 +0300

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,11 +8,20 @@
 
 int is_palindrome(char *s)
 {
-	if (*s == '\0') {
-		return 1;
-	} else if (*s != s[strlen(s) - 1]) {
-		return 0;
-	} else {
-		return is_palindrome(s + 1);
+	int i, j;
+
+	if (s[0] == '\0' || s[1] == '\0')
+		return (1);
+
+	i = 0;
+	j = strlen(s) - 1;
+	while (i < j)
+	{
+		if (s[i] != s[j])
+			return (0);
+		i++;
+		j--;
 	}
+	return (is_palindrome(s + 1, s + j - 1));
 }
+
