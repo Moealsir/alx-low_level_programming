92928e1096b260c3eb464bde63513f5c8ebf0655
Moealsir <mohamedwdalsir@gmail.com>
2023-10-11 23:53:14 +0300

    3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,6 +13,12 @@ int main(int argc, char *argv[])
 {
 	int (*func)(int, int);
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
 	func = get_op_func(argv[2]);
 	if (func == NULL)
 	{
@@ -20,19 +26,6 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit (98);
-	}
-
-	if (strcmp(argv[2], "/") == 0 && atoi(argv[3]) == 0)
-	{
-		fprintf(stderr, "Error\n");
-		exit(100);
-	}
-
 	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
-
 	return (0);
 }
