aca83b406d836f245ab7a686c13c0ffe9266f9fc
Moealsir <mohamedwdalsir@gmail.com>
2023-10-11 23:59:04 +0300

    3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,21 +11,20 @@
 
 int main(int argc, char *argv[])
 {
-	int (*func)(int, int);
+	int a, b, (*op_func)(int, int);
 
 	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		printf("Error\n"), exit(98);
 
-	func = get_op_func(argv[2]);
-	if (func == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+		printf("Error\n"), exit(99);
+
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
+	printf("%d\n", op_func(a, b));
 	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,58 +1,60 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * op_add - add two numbers
- * @a: num 1
- * @b: num 2
- * Return: sum
- */
+ * op_add - add
+ * @a: first int
+ * @b: 2nd int
+ * Return: the sum
+*/
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - sub two numbers
- * @a: num 1
- * @b: num 2
- * Return: sub
- */
+ * op_sub - subtract
+ * @a: first int
+ * @b: 2nd int
+ * Return: the sub
+*/
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - mutliply two numbers
- * @a: num 1
- * @b: num 2
- * Return: mul
- */
+ * op_mul - muliply
+ * @a: first int
+ * @b: 2nd int
+ * Return: the mul
+*/
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - devide two numbers
- * @a: num 1
- * @b: num 2
- * Return: div
- */
+ * op_div - divide
+ * @a: first int
+ * @b: 2nd int
+ * Return: the div
+*/
+
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
 /**
- * op_mod - modulus two numbers
- * @a: num 1
- * @b: num 2
- * Return: mod
- */
+ * op_mod - module
+ * @a: first int
+ * @b: 2nd int
+ * Return: the module
+*/
+
 int op_mod(int a, int b)
 {
 	return (a % b);
