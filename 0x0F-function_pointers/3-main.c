b78256f024b072032722f2a2283a1006a8d0591e
Moealsir <mohamedwdalsir@gmail.com>
2023-10-11 23:23:20 +0300

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,4 +9,22 @@
 
 int main(int argc, char *argv[])
 {
+	int (*func)(int, int);
+
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+
+
 }
