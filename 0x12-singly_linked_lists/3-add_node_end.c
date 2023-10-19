0ffb427dcdcfca32021c24a66d3bafa3251b32e2
Moealsir <mohamedwdalsir@gmail.com>
2023-10-20 02:43:15 +0300

    deleted a.out

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,11 +12,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node = malloc(sizeof(list_t));
 	list_t *node = *head;
 
-	if (!new_node || !(*head))
+	if (!new_node || !*head)
 		return (NULL);
 	if (str)
 	{
-	new_node->str = strdup(str);
+		new_node->str = strdup(str);
 	if (!new_node->str)
 	{
 		free(new_node);
