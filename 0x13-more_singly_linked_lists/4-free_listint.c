9fba40ba4c8a61f2cf57fc4717a8406bd4b07792
Moealsir <mohamedwdalsir@gmail.com>
2023-10-25 04:03:14 +0300

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,18 @@
 #include "lists.h"
 
-/***/
+/**
+ * free_listint - free a list
+ * @head: pointer to first node
+ * Return: void
+*/
 void free_listint(listint_t *head);
 {
-	listint_t *current = head;
+	listint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
 }
