c046f87a47ad24e190f0f434a3b5079ae068db78
Moealsir <mohamedwdalsir@gmail.com>
2023-10-25 03:24:29 +0300

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,15 @@
 #include "lists.h"
 
-/***/
+/**
+ * listint_len - returns the number of elements in a list
+ * @h: pinter to head
+ * Return: len
+*/
 
 size_t listint_len(const listint_t *h)
 {
 	size_t len = 0;
+	
 	while (h != NULL)
 	{
 		h = h->next;
