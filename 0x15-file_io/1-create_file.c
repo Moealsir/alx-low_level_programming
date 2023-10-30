f62dbfe21898d9fbaf838cffee59875a78855835
Moealsir <mohamedwdalsir@gmail.com>
2023-10-30 21:57:11 +0300

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,15 +10,17 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	ssize_t n;
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	if (write(fd, text_content, strlen(text_content)) == -1)
-		return (-1);
+	if (text_content != NULL)
+		n = write(fd, text_content, strlen(text_content));
 
 	close(fd);
-
+	if (n == -1)
+		return (-1);
 	return (1);
 }
