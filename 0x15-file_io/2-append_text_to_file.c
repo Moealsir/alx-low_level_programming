44532cbf1fed7e6172c0975c9031bbd01509967a
Moealsir <mohamedwdalsir@gmail.com>
2023-10-31 02:40:57 +0300

    2-append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,17 +10,18 @@
 int _strlen(char *s)
 {
 	int i = 0;
+
 	if (!s)
 		return (0);
-	while (s[i]!= '\0')
+	while (s[i] != '\0')
 		i++;
 	return (i);
 }
-
 /**
  * append_text_to_file - Appends text at the end of a file.
  * @filename: The name of the file.
  * @text_content: The string to add at the end of the file.
+ * Return: bytes
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -35,5 +36,5 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (len)
 		bytes = write(fd, text_content, len);
 	close(fd);
-	return (bytes == len? 1 : -1);
+	return (bytes == len ? 1 : -1);
 }
