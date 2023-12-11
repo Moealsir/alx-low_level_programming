079886005a5cf38ee1f0acf45b67f92c50c45eb2
Moealsir <mohamedwdalsir@gmail.com>
2023-12-11 22:38:55 +0300

    nrandom.so

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-#!bin/bash
-wget -P /tmp https://github.com/Moealsir/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+#!/bin/bash
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
