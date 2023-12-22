0484b83047f897b3544673ae5646c8e796bbb62d
Moealsir <mohamedwdalsir@gmail.com>
2023-12-23 01:02:00 +0300

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,10 +8,9 @@
  * @size: size of the hash tables
  * Return: key index
  */
-
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	if (key == NULL || size == NULL)
+	if (key == NULL || size == 0)
 		return (0);
 	return (hash_djb2(key) % size);
 }
