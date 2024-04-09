5b62b68691e8de8d0bac1ed4d450944d3d264e8e
Moealsir <mohamedwdalsir@gmail.com>
2024-04-09 16:53:16 +0300

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,13 +9,13 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	size_t i;
 
-	for (i; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
-		return (-1);
 	}
+	return (-1);
 }
